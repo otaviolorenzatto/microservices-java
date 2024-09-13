c5cc99f424242fcf6d37f3c5770ee0848c2a135b
Otavio Augusto Lorenzatto <lorenzattoo@gmail.com>
2024-09-12 21:48:11 -0300

    Criação configs

--- saudacao-service/src/main/java/br/edu/atitus/paradigma/saudacao_service/SaudacaoServiceApplication.java
+++ saudacao-service/src/main/java/br/edu/atitus/paradigma/saudacao_service/SaudacaoServiceApplication.java
@@ -1,7 +1,9 @@
 package br.edu.atitus.paradigma.saudacao_service;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class SaudacaoServiceApplication {
